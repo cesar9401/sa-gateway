69bd5c00e4498532f7ba9ac0cc663647f780b347
César Tzoc <csartalva01@gmail.com>
2024-09-03 22:02:47 -0600

    feat: adding route to sa-organization

--- src/main/java/com/cesar31/gateway/config/WebClientConfiguration.java
+++ src/main/java/com/cesar31/gateway/config/WebClientConfiguration.java
@@ -1,6 +1,8 @@
 package com.cesar31.gateway.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,5 +14,22 @@ public class WebClientConfiguration {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        "sa-organization",
+                        r -> r.path("/api/sa-organization/public/**")
+                                .uri("lb://sa-organization")
+                )
+                .route(
+                        "sa-organization",
+                        r -> r.path("/api/sa-organization/organizations/**")
+                                .uri("lb://sa-organization")
+
+                )
+                .build();
     }
 }
